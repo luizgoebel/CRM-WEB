b0161ce0cc978148fc18f550d595851d559f0d74
Luiz Eduardo Goebel <goebel.adm@gmail.com>
2025-06-21 18:56:58 -0300

    Adicionar controladores e pastas no projeto CRM.Web
    Adicionadas referências a novas pastas no arquivo `CRM.Web.csproj`, incluindo Dtos, ServiceClient e Views para Cliente, Produto e Pedido. Criados os controladores `ClienteController`, `PedidoController` e `ProdutoController`, com métodos `Index` que retornam visões.

---
+++ CRM.WEB/Controllers/ClienteController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.Web.Controllers;
+
+public class ClienteController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ CRM.WEB/Controllers/ProdutoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.Web.Controllers;
+
+public class ProdutoController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
