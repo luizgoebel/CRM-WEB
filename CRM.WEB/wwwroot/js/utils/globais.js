56015ba7dcbc3f93b8fa4ef9d0a1d2dac2e79f1a
Luiz Eduardo Goebel <goebel.adm@gmail.com>
2025-06-22 13:11:59 -0300

    Refatora modais e adiciona feedback visual ao usuário
    - Remove botão "Salvar" do modal em modo de leitura.
    - Reestrutura a barra de navegação no layout para melhor legibilidade.
    - Adiciona wrapper para mensagens de alerta em `_MensagemPartial.cshtml`.
    - Implementa spinner de carregamento em `_SpinnerPartial.cshtml`.
    - Cria funções em `globais.js` para gerenciar exibição de mensagens e spinner.

---
+++ CRM.WEB/wwwroot/js/utils/globais.js
@@ -0,0 +1,26 @@
+﻿function mostrarSpinner() {
+    $('#spinnerGlobal').show();
+}
+
+function esconderSpinner() {
+    $('#spinnerGlobal').hide();
+}
+
+function mostrarMensagem(mensagem) {
+     const classe = 'alert-info';
+
+    const $wrapper = $('#popupMensagemWrapper');
+    const $popup = $('#popupMensagem');
+
+    $popup
+        .removeClass()
+        .addClass(`alert ${classe} mb-0`)
+        .html(mensagem);
+
+    $wrapper.show();
+
+    $('#btnFecharPopup').off('click').on('click', function () {
+        $wrapper.hide();
+    });
+}
+
