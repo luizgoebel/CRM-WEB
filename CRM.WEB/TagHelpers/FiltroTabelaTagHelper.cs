e51cd335ab0c800bb4fac20b773b2cd1ad38b776
Luiz Eduardo Goebel <goebel.adm@gmail.com>
2025-06-28 23:22:49 -0300

    Refatora HTML do TagHelper e ajusta estilos CSS
    Modifica a estrutura HTML no `FiltroTabelaTagHelper.cs` para melhorar a legibilidade, mantendo a funcionalidade. Adiciona interpolação de strings para atributos dinâmicos.
    
    No `site.css`, adiciona regra de mídia para diálogos modais e ajusta a classe `.btn-limpar-filtro` para garantir a consistência de estilos em diferentes estados, incluindo comportamento para filtros ativos e inativos.

--- CRM.WEB/TagHelpers/FiltroTabelaTagHelper.cs
+++ CRM.WEB/TagHelpers/FiltroTabelaTagHelper.cs
@@ -16,28 +16,25 @@ public class FiltroTabelaTagHelper : TagHelper
         string idAttribute = string.IsNullOrEmpty(InputId) ? "" : $"id=\"{InputId}\"";
 
         output.Content.SetHtmlContent($@"
-<div class=""input-group w-auto"">
-    <div class=""position-relative flex-grow-1"">
-        <input type=""text"" 
-               class=""form-control filtro-tabela pe-5""
-               {idAttribute}
-               placeholder=""{Placeholder}""
-               data-filter=""#{TabelaId}""
-               data-controller=""{Controller}""
-               data-tabela-id=""{TabelaId}"" />
-        
-        <button type=""button"" 
-                class=""btn btn-outline-secondary btn-sm btn-limpar-filtro position-absolute""
-                style=""display:none; z-index:2; height: calc(100% - 0.5rem); padding: 0 0.5rem;""
-                data-bs-toggle=""tooltip"" 
-                data-bs-placement=""left""
-                title=""Limpar filtro"">
-            <i class=""fa-solid fa-filter-circle-xmark""></i>
-        </button>
-    </div>
-</div>");
-
-
-
+        <div class=""input-group w-auto"">
+            <div class=""position-relative flex-grow-1"">
+                <input type=""text"" 
+                       class=""form-control filtro-tabela pe-5""
+                       {idAttribute}
+                       placeholder=""{Placeholder}""
+                       data-filter=""#{TabelaId}""
+                       data-controller=""{Controller}""
+                       data-tabela-id=""{TabelaId}"" />
+                
+                <button type=""button"" 
+                        class=""btn btn-outline-secondary btn-sm btn-limpar-filtro position-absolute""
+                        style=""display:none; z-index:2; height: calc(100% - 0.5rem); padding: 0 0.5rem;""
+                        data-bs-toggle=""tooltip"" 
+                        data-bs-placement=""left""
+                        title=""Limpar filtro"">
+                    <i class=""fa-solid fa-filter-circle-xmark""></i>
+                </button>
+            </div>
+        </div>");
     }
 }
