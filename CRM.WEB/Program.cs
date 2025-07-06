737fbc75c321cd5ad6592d83a3c4fb89eb950887
Luiz Eduardo Goebel <goebel.adm@gmail.com>
2025-07-06 04:02:06 -0300

    Adiciona diretiva `using System` e remove configuração de URL
    Adicionada a diretiva `using System;` no arquivo `Program.cs` para permitir o uso de classes do namespace `System`. Removida a configuração do host para URL, que utilizava a variável de ambiente `PORT`, alterando a forma como a aplicação lida com a configuração de URLs ou portas.

--- CRM.WEB/Program.cs
+++ CRM.WEB/Program.cs
@@ -1,12 +1,13 @@
 using CRM.Web;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using System;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 Startup startup = new(builder.Configuration, builder.Environment);
 startup.ConfigureServices(builder.Services);
-
+builder.WebHost.UseUrls($"http://*:{Environment.GetEnvironmentVariable("PORT") ?? "80"}");
 WebApplication app = builder.Build();
 startup.Configure(app);
 
