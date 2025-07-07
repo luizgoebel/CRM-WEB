cabefb95f4ce59a27dc04e16fc4518381785d189
Luiz Eduardo Goebel <goebel.adm@gmail.com>
2025-07-07 16:00:40 -0300

    Atualiza configurações e remove DashboardViewModel
    Modifica o nível de log padrão para "Information" no arquivo `appsettings.Production.json` e adiciona a seção "AppSettings" com a configuração "CrmApi". Remove a configuração "AllowedHosts".
    
    Remove completamente a classe `DashboardViewModel` e suas propriedades associadas.

--- CRM.Web.Models/DashboardViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace CRM.Web.Models;
-
-public class DashboardViewModel
-{
-    public int TotalClientesAtivos { get; set; }
-    public int TotalProdutosEstoque { get; set; }
-    public int TotalPedidosHoje { get; set; }
-}
